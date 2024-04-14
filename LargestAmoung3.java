1c2304789b045b67753a3a3be7b1e0ab491b5552
Vivaan Agarwal <166977816+vivaanopboy@users.noreply.github.com>
2024-04-14 23:28:36 +0530

    Largest number among any three numbers....

--- LargestAmoung3.java
+++ LargestAmoung3.java
@@ -1,1 +1,17 @@
 //Write a program to take three numbers from user and print the largest among them
+import java.util.*;
+class LargestAmoung3{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any three numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if ((a>b) && (a>c))
+            System.out.println(a+" is the largest");
+        else if ((c>b) && (c>a))
+            System.out.println(c+" is the largest");
+        else
+            System.out.println(b+" is the largest");
+    }
+}
